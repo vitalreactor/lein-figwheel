c710df8674ca0ab65fc608496863c4f8e0be3c97
Adrian Medina <adrian.medina@mail.yu.edu>
2015-04-29 14:35:53 -0400

    friendly url not propogating

--- support/src/figwheel/core.clj
+++ support/src/figwheel/core.clj
@@ -63,10 +63,8 @@
     (GET "/figwheel-ws" [] (reload-handler server-state))
     (route/resources "/" :root http-server-root)
     (or ring-handler (fn [r] false))
-    (GET "/" [] (resource-response "index.html" {:root http-server-root}))
-    (if friendly-url?
-      (GET "/*" [] (resource-response "index.html" {:root http-server-root}))
-      (route/not-found "<h1>Page not found</h1>")))
+    (GET "/*" [] (resource-response "index.html" {:root http-server-root}))
+    (route/not-found "<h1>Page not found</h1>"))
    {:port server-port}))
 
 (defn append-msg [q msg]
